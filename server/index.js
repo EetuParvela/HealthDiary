a5af65663e218a9db38fb390ce4d7fcd0c9a6c78
EetuParvela <Eetu.Parvela@metropolia.fi>
2026-03-06 23:35:58 +0200

    Made login page and started on the BMI calc

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import promisePool, { testConnection } from './db-test.js';
+import promisePool, {testConnection} from './db-test.js';
 const app = express();
-const PORT = process.env.BE_PORT;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
@@ -15,19 +15,24 @@ app.listen(PORT, async () => {
 });
 
 app.post('/api/entry', async (req, res) => {
-    const { entry_date, mood_score, notes } = req.body;
+  const {entry_date, mood_score, notes} = req.body;
 
-    try {
-        const sql = "INSERT INTO mood_entries (entry_date, mood_score, notes) VALUES (?, ?, ?)";
+  try {
+    const sql =
+      'INSERT INTO mood_entries (entry_date, mood_score, notes) VALUES (?, ?, ?)';
 
-        const [result] = await promisePool.query(sql, [entry_date, mood_score, notes]);
+    const [result] = await promisePool.query(sql, [
+      entry_date,
+      mood_score,
+      notes,
+    ]);
 
-        res.status(201).json({
-            message: 'Entry saved successfully',
-            id: result.insertId
-        });
-    } catch (error) {
-        console.error('Database Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    res.status(201).json({
+      message: 'Entry saved successfully',
+      id: result.insertId,
+    });
+  } catch (error) {
+    console.error('Database Error:', error);
+    res.status(500).json({error: 'Internal Server Error'});
+  }
 });
